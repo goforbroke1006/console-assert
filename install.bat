010690812ccf0ade87e5b6e137f06dc967ce3a8c
Sergey Cherkesov <s.cherkesov@pbt.com.mt>
2018-11-15 16:05:14 +0300

    Setup build for Win32

--- install.bat
+++ install.bat
@@ -1,16 +1,5 @@
 @ECHO OFF
 
-::call CL -I"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\include" main.cpp
-::call CL -I"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\include" /MT /nologo /TP   /DWIN32 /D_WINDOWS /W3 /GR /EHsc /MDd /Zi /Ob0 /Od /RTC1 /showIncludes /FS -c C:\dev\projects\console-interaction-tester\main.cpp
-
-
-::SET INCLUDE=";C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\ucrt;"
-::SET LIB=";C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\lib;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\ucrt\x64;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\lib\x64;"
-
-::call CL /EHsc -I"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\include" -I"C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\ucrt" C:\dev\projects\console-interaction-tester\main.cpp
-::call CL -I"%INCLUDE%" -L"%LIB%" /EHsc C:\dev\projects\console-interaction-tester\main.cpp
-::call CL /EHsc C:\dev\projects\console-interaction-tester\main.cpp "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\lib\x64\libcpmt.lib" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\lib\x64\libcmt.lib" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\lib\x64\oldnames.lib"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-::call CL /EHsc /Fo%0\..\console-interaction-tester.exe C:\dev\projects\console-interaction-tester\main.cpp
 call CL /EHsc /Fe:"%0\..\console-interaction-tester.exe" C:\dev\projects\console-interaction-tester\main.cpp
 pause
