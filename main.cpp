0f72a3f0d6f374b9545bb9859f190b6d7f3f599d
GoForBroke <go.for.broke1006@gmail.com>
2018-11-21 01:23:20 +0300

    start realize timeout check

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <zconf.h>
 #include <sys/wait.h>
 #include <cstring>
+#include <thread>
+#include <signal.h>
+#include <chrono>
 
 #include "strings.h"
 
@@ -9,6 +12,9 @@ constexpr auto PIPE_READ = 0;
 constexpr auto PIPE_WRITE = 1;
 
 using namespace std;
+using namespace std::chrono;
+
+constexpr int MILLISECONDS_TO_MICROSECONDS = 1000;
 
 int main(int argc, char **argv) {
 
@@ -17,18 +23,21 @@ int main(int argc, char **argv) {
             || strcmp(argv[1], "--help") == 0
             || strcmp(argv[1], "-h") == 0
     )) {
-        cout << "Usage: ./console-interaction-tester SOME_BIN_TEST_SUBJECT TEST_INPUT EXPECTED_OUTPUT";
+        cout << "Usage: ./console-interaction-tester "
+                "<SOME_BIN_TEST_SUBJECT> <TEST_INPUT> <EXPECTED_OUTPUT> <TIMEOUT_IN_MILLISECONDS>";
         return 0;
     }
 
     string binFilename;
     string testInput;
     string expectedOutput;
+    unsigned int timeout = 0;
 
     if (argc >= 4) {
         binFilename = argv[1];
         testInput = argv[2];
         expectedOutput = argv[3];
+        timeout = atoi(argv[4]);
     } else {
         cout << "Enter subject binary path: ";
         getline(cin, binFilename);
@@ -38,6 +47,9 @@ int main(int argc, char **argv) {
 
         cout << "Enter expected output: ";
         getline(cin, expectedOutput);
+
+        cout << "Timeout: ";
+        cin >> timeout;
     }
 
     int inputFd[2];
@@ -66,8 +78,12 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
-//    int status = 0;
+    int status;
     string result;
+    pid_t pid;
+
+    milliseconds timeBefore;
+    milliseconds timeAfter;
 
     if (childPid == 0) {
 //        printf("I'm child %d, parent is %d\n", getpid(), getppid());
@@ -86,12 +102,16 @@ int main(int argc, char **argv) {
         close(outputFd[PIPE_READ]);
         close(outputFd[PIPE_WRITE]);
 
+        timeBefore = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+
         const char *szCommand = binFilename.data();
         char *const aArguments[] = {(char *const) binFilename.data(), nullptr};
         char *const aEnvironment[] = {nullptr};
         int nResult = execve(szCommand, aArguments, aEnvironment);
         exit(nResult);
     }
+
+    thread cThread;
 
     if (childPid > 0) {
 //        printf("I'm paretn %d, child is %d\n", getpid(), childPid);
@@ -102,6 +122,11 @@ int main(int argc, char **argv) {
 //        waitpid(childPid, &status, WNOHANG);
 //        printf("Child ready for input\n");
 
+//        if ((pid = wait(&status)) < 0) {
+//            perror("wait");
+//            exit(1);
+//        }
+
         const char *input = (testInput + "\n").c_str();
         write(inputFd[PIPE_WRITE], input, strlen(input));
         while (read(outputFd[PIPE_READ], &nChar, 1) == 1) {
@@ -109,14 +134,17 @@ int main(int argc, char **argv) {
         }
     }
 
-    result = trim(result);
+//    wait(nullptr);
 
-    /*if (result.length() > 0) {
-        const string &last = result.substr(result.length() - 1, 1);
-        if (last == "\n") {
-            result = result.substr(0, result.length() - 1);
-        }
-    }*/
+    timeAfter = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+
+    const milliseconds &delta = timeAfter - timeBefore;
+    if (delta.count() > timeout) {
+        cerr << "time limit exceeded (" << delta.count() << ")" << endl;
+        return -1;
+    }
+
+    result = trim(result);
 
 //    waitpid(childPid, &status, WUNTRACED);
 //    printf("Child finish with status %d\n", status);
