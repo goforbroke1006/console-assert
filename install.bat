ac79517433997f2474c4845a8d1a632d4a327858
Sergey Cherkesov <s.cherkesov@pbt.com.mt>
2018-11-21 18:14:56 +0300

    fix

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-CALL cl /EHsc /Fe:%0\..\console-interaction-tester.exe C:\dev\projects\console-interaction-tester\main.cpp
+CALL cl /EHsc /Fe:%0\..\console-interaction-tester.exe %0\..\main_win32.cpp
 PAUSE
