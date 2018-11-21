f31736c850bc73cf640fc4accfa235cd4338185d
Sergey <go.for.broke1006@gmail.com>
2018-11-21 18:21:17 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-CALL cl /EHsc /Fe:%0\..\console-interaction-tester.exe %0\..\main_win32.cpp
+CALL cl /EHsc /Fe:%0\..\console-assert.exe %0\..\main_win32.cpp
 PAUSE
