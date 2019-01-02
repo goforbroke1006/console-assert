8e82416fbcf4fe490df23334224dc271d5fb35a3
GoForBroke <go.for.broke1006@gmail.com>
2019-01-02 11:45:11 +0300

    update script

--- install.sh
+++ install.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
+#PLATFORM=`uname`
+
+case "$OSTYPE" in
+  solaris*) PLATFORM="SOLARIS" ;;
+  darwin*)  PLATFORM="OSX" ;;
+  linux*)   PLATFORM="LINUX" ;;
+  bsd*)     PLATFORM="BSD" ;;
+  msys*)    PLATFORM="WINDOWS" ;;
+  *)        PLATFORM="unknown: $OSTYPE" ;;
+esac
+
+
 rm -rf build
 mkdir build
 cd build
 cmake ..
 make
-make install
+
+if [[ "$PLATFORM" == 'LINUX' ]]; then
+    sudo make install
+else
+    make install
+fi
 
 echo 'Press any key...'
 read
